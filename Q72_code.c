fc496340641b4f84277550cded6a7254bc95c57f
shubhtyagi01 <shubhtyagi8004@gmail.com>
2025-09-30 21:13:54 +0530

    100 days code

---
+++ Q72_code.c
@@ -0,0 +1,30 @@
+//Find the sum of all elements in a matrix.
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols, sum = 0;
+
+    
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+    int matrix[100][100]; 
+
+    
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+            sum += matrix[i][j]; 
+        }
+    }
+
+    
+    printf("\nSum of all elements in the matrix: %d\n", sum);
+
+    return 0;
+}
