6f5f2dc72d28bc15c1241d507777f6d4d27c7b7d
shubhtyagi01 <shubhtyagi8004@gmail.com>
2025-10-04 22:37:35 +0530

    100 days code

---
+++ Q80_code.c
@@ -0,0 +1,64 @@
+//Multiply two matrices.
+
+#include <stdio.h>
+
+#define MAX 100
+
+int main() {
+    int A[MAX][MAX], B[MAX][MAX], result[MAX][MAX];
+    int r1, c1, r2, c2;
+
+    
+    printf("Enter rows and columns for Matrix A: ");
+    scanf("%d %d", &r1, &c1);
+    printf("Enter rows and columns for Matrix B: ");
+    scanf("%d %d", &r2, &c2);
+
+    
+    if (c1 != r2) {
+        printf("Matrix multiplication not possible: columns of A must equal rows of B.\n");
+        return 1;
+    }
+
+    printf("Enter elements of Matrix A:\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    
+    printf("Enter elements of Matrix B:\n");
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            result[i][j] = 0;
+        }
+    }
+
+    
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) {
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    
+    printf("Resultant Matrix (A x B):\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
