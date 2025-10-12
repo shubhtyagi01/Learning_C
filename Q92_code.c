061e5f269c927744f1ff7a8c3fdb1fb389dc82f8
shubhtyagi01 <shubhtyagi8004@gmail.com>
2025-10-12 18:53:13 +0530

    100 days code

---
+++ Q92_code.c
@@ -0,0 +1,28 @@
+//Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include <stdio.h>
+
+int main() {
+    float num1, num2;
+
+    
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    
+    printf("Sum: %.2f\n", num1 + num2);
+    printf("Difference: %.2f\n", num1 - num2);
+    printf("Product: %.2f\n", num1 * num2);
+
+    
+    if (num2 != 0) {
+        printf("Quotient: %.2f\n", num1 / num2);
+    } else {
+        printf("Quotient: Division by zero is not allowed.\n");
+    }
+
+    return 0;
+}
